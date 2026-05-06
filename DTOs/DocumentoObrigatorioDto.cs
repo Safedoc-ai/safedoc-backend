635e45fb8ea66f561ea445595519cbbed062d0f8
AiltonJ303 <ailtonjose303@outlook.com>
2026-05-05 22:01:26 -0300

    Atualiza integração do front com backend

---
+++ DTOs/DocumentoObrigatorioDto.cs
@@ -0,0 +1,8 @@
+namespace SafeDocAI.API.DTOs;
+
+public class DocumentoObrigatorioDto
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string Frequencia { get; set; } = string.Empty;
+}
